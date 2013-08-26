f08c64d18d3809858f65c7be30acdabfd693cd62
david jones <zell71@hotmail.com>
2013-08-26 14:47:26 +1000

    test commit

--- TestCIApplication/Controllers/HomeController.cs
+++ TestCIApplication/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestCIApplication.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. test";
 
             return View();
         }
